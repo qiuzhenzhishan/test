11ca431e6b24ea036f2dbb38bbc6d6a37731909f
qiuzhenzhishan <784041850@qq.com>
2018-11-16 21:51:31 +0800

    添加注释

---
+++ java/cn/dao/User.java
@@ -0,0 +1,22 @@
+package cn.dao;
+
+public class User {
+    private int num;
+    private String name;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
