14145d791c853066c0a308d7794bd6c0415280b3
qiuzhenzhishan <784041850@qq.com>
2018-11-16 21:54:49 +0800

    添加注释

---
+++ java/cn/dao/Role.java
@@ -0,0 +1,13 @@
+package cn.dao;
+
+public class Role {
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
